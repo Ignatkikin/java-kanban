5960b6953676a54bbcf00350f4efe2dfbc0a6573
Ignatkick <ignatkikin@yandex.ru>
2024-05-10 21:09:22 +0800

    Спринт 6, первая проверка

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,25 +3,89 @@ package manager;
 import tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private List<Task> history = new ArrayList<>();
-    private static final int MAX_HISTORY_SIZE = 10;
+
+    private Map<Integer, Node> history = new HashMap<>();
+    private Node head;
+    private Node tail;
+
+    private Node linkLast(Task task) {
+        final Node oldTail = tail;
+        final Node newNode = new Node(task, null, oldTail);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        return newNode;
+    }
+
+    private void removeNode(Node removeNode) {
+        if (removeNode != null) {
+            final Node next = removeNode.next;
+            final Node prev = removeNode.prev;
+
+            if (prev == null) {
+                head = next;
+            } else {
+                prev.next = next;
+                removeNode.prev = null;
+            }
+
+            if (next == null) {
+                tail = prev;
+            } else {
+                next.prev = prev;
+                removeNode.next = null;
+            }
+        }
+    }
+
+    private List<Task> getTasks() {
+        List<Task> historyList = new ArrayList<>();
+        Node node = head;
+
+        while (node != null) {
+            historyList.add(node.data);
+            node = node.next;
+        }
+        return historyList;
+    }
 
     @Override
     public void add(Task task) {
         if (task != null) {
-            if (history.size() == MAX_HISTORY_SIZE) {
-                history.remove(0);
-            }
-            history.add(task);
+            removeNode(history.get(task.getId()));
+            history.put(task.getId(), linkLast(task));
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        return getTasks();
     }
 
+    @Override
+    public void remove(int id) {
+        removeNode(history.get(id));
+        history.remove(id);
+    }
+
+    private static class Node {
+        Task data;
+        Node next;
+        Node prev;
+
+        public Node(Task data, Node next, Node prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
 }
--- test/tasks/SubtaskTest.java
+++ test/tasks/SubtaskTest.java
@@ -1,6 +1,7 @@
 package tasks;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
